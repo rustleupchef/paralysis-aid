0187463ea0028d9eb22fb7b24ad5e32f14955095
rustleupchef <rustle.up.chef@gmail.com>
2025-09-03 21:16:09 -0500

    changed detect script

--- detect.py
+++ detect.py
@@ -1,14 +1,20 @@
 import json
 import joblib
 import pandas as pd
+import requests
+from time import sleep
 
 model = joblib.load("models/thought_classifier.pkl")
 scalar = joblib.load("models/scaler.pkl")
 
-with open("test.json") as f:
-    new_data = json.load(f)
+url: str = "http://localhost:3000/mindwave/data"
 
-df_new = pd.DataFrame(new_data)
-x_new_scaled = scalar.transform(df_new)
-predictions = model.predict(x_new_scaled)
-print(predictions)
+while True:
+    response = requests.get(url)
+    print(response.text)
+    new_data = [json.loads(response.text)["eeg"]]
+    df_new = pd.DataFrame(new_data)
+    x_new_scaled = scalar.transform(df_new)
+    predictions = model.predict(x_new_scaled)
+    print(predictions)
+    sleep(0.5)
