b1d3d607f2b3139423f67ff0590a829b38551440
rustleupchef <rustle.up.chef@gmail.com>
2025-10-04 22:09:22 -0500

    made the server render a basic frame on a web page for better training

--- server/server.js
+++ server/server.js
@@ -12,6 +12,26 @@ const server = http.createServer((req, res) => {
   if (req.url === '/mindwave/data' && req.method === 'GET') {
     res.writeHead(200, { 'Content-Type': 'application/json' });
     res.end(JSON.stringify(latestMindwaveData));
+  } else if (req.url === '/collect' && req.method === "GET") {
+    const config = require("../Collecting/config.json");
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.end(`<div style="display: flex; position: absolute; left: 0; top: 0; width: 100%; height: 100%; justify-content: center; align-items: center; flex-direction: column;"><h4>${config.className}</h4><h4>${config.className}</h4><h4>${config.className}</h4><h4>${config.className}</h4><h4>${config.className}</h4><h4>${config.className}</h4><h4>${config.className}</h4><h4>${config.className}</h4><h4>${config.className}</h4><h4>${config.className}</h4><h4>${config.className}</h4></div>`);
+  } else if (req.url === "/detect" && req.method === "GET") {
+    const classes = require("../Classes/Formatted/ClassKey.json");
+    const classArray = Object.values(classes);
+    let text = `<div style="display: flex; position: absolute; left: 0; top: 0; width: 100%; height: 100%; justify-content: center; align-items: center; flex-direction: column;">`;
+    for (let i = 0; i < 10; i++) {
+      let innerText = `<div style="display: flex; width: 100%; justify-content: center; align-items: center; flex-direction: row;">`;
+      classArray.forEach((e) => {
+        innerText += `<h4 style="margin: 0; padding: 4vw; text-align: center;">${e}</h4>`;
+      });
+      innerText += "</div>";
+      text += innerText;
+    }
+    text += "</div>";
+    console.log(text);
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.end(text);
   } else {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
     res.end('Mindwave server is running\n');
@@ -22,7 +42,6 @@ const PORT = 3000;
 server.listen(PORT, () => {
   console.log(`Server is listening on port ${PORT}`);
 });
-
 
 const mw = new Mindwave();
 mw.on('eeg', data => {
