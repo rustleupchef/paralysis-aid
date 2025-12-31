b9404caa6c14acb73e3042cc8ab1ce5ee3dcadb3
rustleupchef <rustle.up.chef@gmail.com>
2025-12-31 14:32:41 -0600

    formatted server pages into proper html files, and added extra POST endpoints

--- server/server.js
+++ server/server.js
@@ -22,26 +22,37 @@ const server = http.createServer((req, res) => {
     if (req.url === '/mindwave/data' && req.method === 'GET') {
         res.writeHead(200, { 'Content-Type': 'application/json' });
         res.end(JSON.stringify(latestMindwaveData));
-    } else if (req.url === '/collect' && req.method === "GET") {
+    } else if (req.url === '/collect' && req.method === "POST") {
         const config = require("../Collecting/config.json");
-        res.writeHead(200, { 'Content-Type': 'text/html' });
-        res.end(`<div style="display: flex; position: absolute; left: 0; top: 0; width: 100%; height: 100%; justify-content: center; align-items: center; flex-direction: column;"><h4>${config.className}</h4><h4>${config.className}</h4><h4>${config.className}</h4><h4>${config.className}</h4><h4>${config.className}</h4><h4>${config.className}</h4><h4>${config.className}</h4><h4>${config.className}</h4><h4>${config.className}</h4><h4>${config.className}</h4><h4>${config.className}</h4></div>`);
-    } else if (req.url === "/detect" && req.method === "GET") {
+        res.writeHead(200, { 'Content-Type': 'application/json' });
+        res.end(JSON.stringify({ class_name: config.className }));
+    } else if (req.url === '/collect' && req.method === "GET") {
+        const filePath = path.join(__dirname, "collect.html");
+        fs.readFile(filePath, (err, data) => {
+            if (err) {
+                res.writeHead(500, { 'Content-type': 'text/plain' });
+                res.end("Error loading content");
+                return;
+            }
+            res.writeHead(200, { 'Content-type': "text/html" });
+            res.end(data);
+        });
+    } else if (req.url === "/detect" && req.method === "POST") {
         const classes = require("../Classes/Formatted/Key.json");
         const classArray = Object.values(classes.classes);
-        let text = `<div style="display: flex; position: absolute; left: 0; top: 0; width: 100%; height: 100%; justify-content: center; align-items: center; flex-direction: column;">`;
-        for (let i = 0; i < 10; i++) {
-            let innerText = `<div style="display: flex; width: 100%; justify-content: center; align-items: center; flex-direction: row;">`;
-            classArray.forEach((e) => {
-                innerText += `<h4 style="margin: 0; padding: 4vw; text-align: center;">${e}</h4>`;
-            });
-            innerText += "</div>";
-            text += innerText;
-        }
-        text += "</div>";
-        console.log(text);
-        res.writeHead(200, { 'Content-Type': 'text/html' });
-        res.end(text);
+        res.writeHead(200, { 'Content-Type': 'application/json' });
+        res.end(JSON.stringify({ classes: classArray }));
+    } else if (req.url === "/detect" && req.method === "GET") {
+        const filePath = path.join(__dirname, "detect.html");
+        fs.readFile(filePath, (err, data) => {
+            if (err) {
+                res.writeHead(500, { 'Content-type': 'text/plain' });
+                res.end("Error loading content");
+                return;
+            }
+            res.writeHead(200, { 'Content-type': "text/html" });
+            res.end(data);
+        });
     } else if (req.url === "/mindwave/cv" && req.method === 'POST') {
         let body = '';
         req.on('data', chunk => {
@@ -84,7 +95,7 @@ const server = http.createServer((req, res) => {
         res.writeHead(200, { 'Content-Type': 'application/json' });
         res.end(JSON.stringify(latestDetection));
     } else if (req.url === "/mindwave/display" && req.method === "GET") {
-        const filePath = path.join(__dirname, "results.html");
+        const filePath = path.join(__dirname, "display.html");
         fs.readFile(filePath, (err, data) => {
             if (err) {
                 res.writeHead(500, { 'Content-type': 'text/plain' });
