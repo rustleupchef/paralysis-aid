8e65576bb89bc2a4786cb69a8d4e0c3c8a55a1b2
rustleupchef <rustle.up.chef@gmail.com>
2026-01-06 23:08:42 -0600

    added a more nuanced typing system

--- Handler/src/App.java
+++ Handler/src/App.java
@@ -1,10 +1,15 @@
 import java.io.BufferedReader;
+import java.io.FileReader;
 import java.io.InputStreamReader;
-import java.io.OutputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.HashMap;
+import java.awt.Robot;
+import java.awt.Toolkit;
+import java.awt.datatransfer.StringSelection;
+import java.awt.event.KeyEvent;
 
-import com.fazecast.jSerialComm.SerialPort;
+
 import com.google.gson.Gson;
 
 class jsonObject {
@@ -27,17 +32,83 @@ class jsonObject {
     }
 }
 
+class typingObject {
+    public String[] vertical;
+    public String[] horizontal;
+    public String[] blink;
+    public String[] smirk;
+    public String mouth;
+    public HashMap<String, String> eeg;
+
+    typingObject() {
+
+    }
+
+    typingObject (String[] vertical, String[] horizontal, String[] blink, String[] smirk, String mouth, HashMap<String, String> eeg) {
+        this.vertical = vertical;
+        this.horizontal = horizontal;
+        this.blink = blink;
+        this.smirk = smirk;
+        this.mouth = mouth;
+        this.eeg = eeg;
+    }
+}
+
+class transferObject {
+    public String vertical;
+    public String horizontal;
+    public String leftBlink;
+    public String rightBlink;
+    public String leftSmirk;
+    public String rightSmirk;
+    public String mouth;
+    public String eeg;
+
+    transferObject() {
+
+    }
+
+    transferObject (jsonObject reading, typingObject Typing) {
+        String[] dirParts = reading.dir.split("-");
+        int horiz = dirParts[0].toLowerCase().equals("left") ? 0 : dirParts[0].toLowerCase().equals("right") ? 1 : 2;
+        int vert = dirParts[1].toLowerCase().equals("up") ? 0 : dirParts[1].toLowerCase().equals("down") ? 1 : 2;
+
+        this.vertical = vert < 2 ? Typing.vertical[vert] : "";
+        this.horizontal = horiz < 2 ? Typing.horizontal[horiz] : "";
+
+        this.leftBlink = reading.squint[0].equals("True") ? Typing.blink[0] : "";
+        this.rightBlink = reading.squint[1].equals("True") ? Typing.blink[1] : "";
+
+        this.leftSmirk = reading.smirk[0].equals("True") ? Typing.smirk[0] : "";
+        this.rightSmirk = reading.smirk[1].equals("True") ? Typing.smirk[1] : "";
+
+        this.mouth = reading.open_mouth ? Typing.mouth : "";
+
+        this.eeg = Typing.eeg.getOrDefault(reading.eeg_class, "");
+    }
+
+    transferObject (String vertical, String horizontal, String leftBlink, String rightBlink, String leftSmirk, String rightSmirk, String mouth, String eeg) {
+        this.vertical = vertical;
+        this.horizontal = horizontal;
+        this.leftBlink = leftBlink;
+        this.rightBlink = rightBlink;
+        this.leftSmirk = leftSmirk;
+        this.rightSmirk = rightSmirk;
+        this.mouth = mouth;
+        this.eeg = eeg;
+    }
+
+    public String condense() {
+        return this.vertical + this.horizontal + this.leftBlink + this.rightBlink + this.leftSmirk + this.rightSmirk + this.mouth + this.eeg;
+    }
+}
+
 public class App {
     public static void main(String[] args) throws Exception {
-        SerialPort port = SerialPort.getCommPort("/dev/ttyACM0");
-        if (!port.openPort()) {
-            System.out.println("Port didn't open succesfully");
-            return;
-        }
-        port.setBaudRate(9600);
-        Thread.sleep(2000);
 
-        OutputStream out = port.getOutputStream();
+        Robot robot = new Robot();
+        robot.setAutoDelay(40);
+        typingObject Typing = new Gson().fromJson(new FileReader("typing.json"), typingObject.class);
 
         while (true) {
             URL url = new URL("http://localhost:3000/mindwave/detection");
@@ -60,26 +131,39 @@ public class App {
                 continue;
             }
 
-            if (!port.isOpen()) {
-                System.out.println("Port disconnected, attempting to reconnect...");
-                if (!port.openPort()) {
-                    System.out.println("Failed to reconnect");
-                    continue;
-                }
-                out = port.getOutputStream();
-            }
-            
-            try {
-                System.out.println(reading.smirk[0]);
-                out.write(reading.smirk[0].equals("True") ? 1 : 0);
-                out.flush();
-            } catch (Exception e) {
-                System.out.println("Write failed: " + e.getMessage());
-                if (port.isOpen()) {
-                    port.closePort();
-                }
-            }
-            Thread.sleep(1000);
+            transferObject transfer = new transferObject(reading, Typing);
+            System.out.println(transfer.condense());
+            typeString(robot, transfer.condense());
         }
+    }
+
+    public void waylandTypeString(String text) throws InterruptedException {
+        StringSelection selection = new StringSelection(text);
+        Toolkit.getDefaultToolkit().getSystemClipboard().setContents(selection, null);
+    }
+
+    public static void typeString(Robot robot, String text) throws InterruptedException {
+        System.out.println(text);
+        for (char character : text.toCharArray()) {
+            System.out.println(character);
+            int keyCode = KeyEvent.getExtendedKeyCodeForChar(character);
+            
+            if (Character.isUpperCase(character) || needsShift(character)) {
+                robot.keyPress(KeyEvent.VK_SHIFT);
+                robot.keyPress(keyCode);
+                robot.keyRelease(keyCode);
+                robot.keyRelease(KeyEvent.VK_SHIFT);
+            } else {
+                robot.keyPress(keyCode);
+                robot.keyRelease(keyCode);
+            }
+            robot.delay(40); 
+        }
+    }
+
+    private static boolean needsShift(char c) {
+        return c == '!' || c == '@' || c == '#' || c == '$' || c == '%' || c == '^' || 
+               c == '&' || c == '*' || c == '(' || c == ')' || c == ':' || c == '"' || 
+               c == '?' || c == '<' || c == '>';
     }
 }
